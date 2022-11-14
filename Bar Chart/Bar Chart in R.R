645731cfbbc1c9b5c4e16d19603a21ab9d68a949
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-14 13:40:40 +0300

    Update Bar Chart in R.R

--- Bar Chart/Bar Chart in R.R
+++ Bar Chart/Bar Chart in R.R
@@ -7,6 +7,13 @@ df_used_cars <- read.csv("C:/Users/sbyeg/Downloads/bar_chart_data.csv",
 bar_chart <- ggplot(df_used_cars, 
                     aes(x = Brand, 
                         y = Cars.Listings)) +
-                        geom_bar(stat = "identity")
+                        geom_bar(stat = "identity" ,
+                                 color = "navy", 
+                                 fill = "navy") +
+  ggtitle("Cars Listings by Brand") + 
+  theme_classic() + theme(axis.text.x = element_text(angle = 45,
+                                                     hjust = 1))  +
+  labs(x = NULL,
+       y = "Number of Listings")
 
 bar_chart
