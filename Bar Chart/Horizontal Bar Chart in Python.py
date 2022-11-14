2497d6dac73495af7304b0745236faebad30d39f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-14 14:30:35 +0300

    Update Horizontal Bar Chart in Python.py

--- Bar Chart/Horizontal Bar Chart in Python.py
+++ Bar Chart/Horizontal Bar Chart in Python.py
@@ -23,3 +23,32 @@ plt.ylabel("Number of Listings", fontsize = 13 )
 plt.show()
 # To save your plot as an image
 plt.savefig("Used Cars Bar.png")
+
+#Try this as well
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set() #This overides the matplotib look, to take advantage of the seaborn styling
+
+#Always remember to use forward "/" slashes when specifying a path to a file.
+df_used_cars = pd.read_csv("C:/Users/sbyeg/Downloads/bar_chart_data.csv")
+
+#After loading the data frame to check what it contains
+df_used_cars
+
+#to translate this data in the form of a bar chart
+plt.figure(figsize = (9, 6)) #To increase the size of the plot, to make the x-axis labels readable and not overlap
+plt.barh(x = df_used_cars["Cars Listings"],
+        height = df_used_cars["Brand"],
+       color = "midnightblue")# color = "rgbwymc" -Assigns each column an individual color
+
+#To rotate the x-axis labes at an angle
+plt.xticks(rotation = 45, fontsize = 13)
+plt.yticks(fontsize = 13)
+plt.title("Cars Listings by Brand", fontsize = 16, fontweight = "bold")
+plt.ylabel("Number of Listings", fontsize = 13 )
+plt.show()
+# To save your plot as an image
+plt.savefig("Used Cars Bar.png")
