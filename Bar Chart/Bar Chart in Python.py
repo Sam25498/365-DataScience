46edfe4942adf4249d3b25b0cb391bfe9890efa4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-14 12:45:34 +0300

    Update Bar Chart in Python.py

--- Bar Chart/Bar Chart in Python.py
+++ Bar Chart/Bar Chart in Python.py
@@ -1,2 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#Always remember to use forward "/" slashes when specifying a path to a file.
+df_used_cars = pd.read_csv("C:/Users/sbyeg/Downloads/bar_chart_data.csv")
