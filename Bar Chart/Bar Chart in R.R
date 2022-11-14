e3a27a3800466afaa1ea88d22c13ca3e4db995cb
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-14 13:21:36 +0300

    Update Bar Chart in R.R

--- Bar Chart/Bar Chart in R.R
+++ Bar Chart/Bar Chart in R.R
@@ -1,3 +1,3 @@
 df_used_cars <- read.csv("C:/Users/sbyeg/Downloads/bar_chart_data.csv", 
-                         header = True,
+                         header = TRUE,
                          sep = ",")
