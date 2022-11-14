8cc782f48316bae24f602ee27179b2a6207a2623
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-14 12:52:53 +0300

    Update Bar Chart in Python.py

--- Bar Chart/Bar Chart in Python.py
+++ Bar Chart/Bar Chart in Python.py
@@ -8,7 +8,10 @@ df_used_cars = pd.read_csv("C:/Users/sbyeg/Downloads/bar_chart_data.csv")
 df_used_cars
 
 #to translate this data in the form of a bar chart
+plt.figure(figsize = (9, 6)) #To increase the size of the plot, to make the x-axis labels readable and not overlap
 plt.bar(x = df_used_cars["Brand"],
         height = df_used_cars["Cars Listings"])
 
+#To rotate the x-axis labes at an angle
+plt.xticks(rotation = 45)
 plt.show()
