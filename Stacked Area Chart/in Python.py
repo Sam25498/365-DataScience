30a44cbc9ecedab803306a0ad788360b1256955f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-15 16:11:54 +0300

    Update in Python.py

--- Stacked Area Chart/in Python.py
+++ Stacked Area Chart/in Python.py
@@ -6,3 +6,14 @@ sns.set()
 df_fuel_engine_types = pd.read_csv("stacked_area_chart_data.csv")
 
 print(df_fuel_engine_types)
+
+colors = ["011638", "#7e2987", "#ef2026"]
+plt.figure(figsize = (12, 6))
+plt.stackplot(df_fuel_engine_types["Year"],
+              df_fuel_engine_types["Diesel"],
+              df_fuel_engine_types["Petrol"],
+              df_fuel_engine_types["Gas"],
+             colors = colors,
+             edgecolor = 'none') #'edgecolor' helps control the border colors
+plt.xticks(df_fuel_engine_types["Year"], rotation = 45)
+plt.show()
