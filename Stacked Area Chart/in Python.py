f82dae9bde45017b7d950ca600fa1889c20edd95
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-15 16:05:33 +0300

    Update in Python.py

--- Stacked Area Chart/in Python.py
+++ Stacked Area Chart/in Python.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+
+df_fuel_engine_types = pd.read_csv("stacked_area_chart_data.csv")
+
+print(df_fuel_engine_types)
