cdd8451bbed9546300b5e216867b0937ba3c1c68
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-15 13:44:44 +0300

    Update Creating a Pie Chart in R.R

--- Pie Chart/Creating a Pie Chart in R.R
+++ Pie Chart/Creating a Pie Chart in R.R
@@ -4,4 +4,12 @@ df_fuel_types <- read.csv('C:/Users/sbyeg/Downloads/pie_chart_data.csv', header 
 
 df_fuel_types
 
+pie_chart <- ggplot(df_fuel_types, 
+                    aes(x = "",
+                        y = Number.of.Cars, 
+                        fill = Engine.Fuel.Type)) +
+                    geom_bar(stat = "identity",
+                             width = 1) + 
+                    coord_polar("y")
+pie_chart
 
