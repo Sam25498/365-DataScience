be00164e4de50472049c9e35937036d7f87506e2
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-16 15:32:27 +0300

    Create in Python.py

---
+++ Line Chart/in Python.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+sns.set()
+
+df_spx_ftse_00_10 = pd.read_csv("C:/Users/sbyeg/Downloads/line_chart_data.csv")
+df_spx_ftse_00_10
