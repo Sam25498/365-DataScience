91ddc194107720c7d40b565abd35ecf5c3ecb621
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-16 23:34:59 +0300

    Update in Python.py

--- Histogram/in Python.py
+++ Histogram/in Python.py
@@ -4,3 +4,14 @@ import seaborn as sns
 sns.set()
 
 df_real_estate = pd.read_csv("C:/Users/sbyeg/Downloads/histogram_data.csv")
+
+print(df_real_estate)
+
+#For our histogram we only require one the columns which is price
+plt.hist(df_real_estate["Price"])
+plt.show()
+
+#Choosing the appropriate number of bins, or the bin size is the most crucial element of every histogram
+#Through varying bin sizes a histogram can reveal vastly different insights
+
+
