9f565d13fceb5966a206e6fbd44660fe16b16737
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-16 15:37:24 +0300

    Update in Python.py

--- Line Chart/in Python.py
+++ Line Chart/in Python.py
@@ -4,4 +4,8 @@ import seaborn as sns
 sns.set()
 
 df_spx_ftse_00_10 = pd.read_csv("C:/Users/sbyeg/Downloads/line_chart_data.csv")
+print(df_spx_ftse_00_10)
+
+df_spx_ftse_00_10["new_date"] = pd.to_datetime(df_spx_ftse_00_10["Date"]) # This transforms the date data in a format that python can work with.
 df_spx_ftse_00_10
+
