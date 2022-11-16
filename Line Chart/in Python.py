b984ad30040002971b29e22806bb55e05bf25f67
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-16 15:39:45 +0300

    Update in Python.py

--- Line Chart/in Python.py
+++ Line Chart/in Python.py
@@ -9,3 +9,9 @@ print(df_spx_ftse_00_10)
 df_spx_ftse_00_10["new_date"] = pd.to_datetime(df_spx_ftse_00_10["Date"]) # This transforms the date data in a format that python can work with.
 df_spx_ftse_00_10
 
+plt.plot(df_spx_ftse_00_10["new_date"], df_spx_ftse_00_10["GSPC500"])
+plt.plot(df_spx_ftse_00_10["new_date"], df_spx_ftse_00_10["FTSE100"])
+plt.show()
+
+
+
