d3644194c1aad421ff3e74f811d1ebf156c45f69
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 13:48:12 +0300

    Update in Python.py

--- Scatter Plot/in Python.py
+++ Scatter Plot/in Python.py
@@ -8,6 +8,10 @@ print(df_real_estate)
 
 plt.figure(figsize = (12,8))
 plt.scatter(df_real_estate['Area (ft.)'],
-            df_real_estate['Price'])
+            df_real_estate['Price'],
+           alpha = 0.6,
+            c = df_real_estate['Building Type']
+           )
 plt.show()
-
+#Having a large data set might result in overplotting, to avoid this issue we can add a transparency level, "alpha": controls transparency level and takes values from 0 to 1
+#in a scatter plot we can color code according to a third variable, in this way were are able to incorporate much more info in a scatter plot, e.g c = df_real_estate['Building Type']
