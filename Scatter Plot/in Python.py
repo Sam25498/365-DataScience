f68589a6da3c8ded4751b7b9bc66230cdbb35f44
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 13:42:05 +0300

    Update in Python.py

--- Scatter Plot/in Python.py
+++ Scatter Plot/in Python.py
@@ -2,3 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+
+df_real_estate = pd.read_csv("C:/Users/sbyeg/Downloads/scatter_data.csv")
+print(df_real_estate)
+
+plt.figure(figsize = (12,8))
+plt.scatter(df_real_estate['Area (ft.)'],
+            df_real_estate['Price'])
+plt.show()
+
