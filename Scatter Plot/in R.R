cae2d4f009f29c4ce86bc9952de1cee77875b4c4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 14:22:36 +0300

    Create in R.R

---
+++ Scatter Plot/in R.R
@@ -0,0 +1,6 @@
+library("ggplot2")
+library("wesanderson")
+
+df_real_estate <- read.csv("C:/Users/sbyeg/Downloads/scatter_data.csv",
+                          header = TRUE,
+                          sep = ",")
