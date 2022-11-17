a862268189aa75dc4cd94c14d472266e56cb4393
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 14:22:56 +0300

    Update in R.R

--- Scatter Plot/in R.R
+++ Scatter Plot/in R.R
@@ -4,3 +4,8 @@ library("wesanderson")
 df_real_estate <- read.csv("C:/Users/sbyeg/Downloads/scatter_data.csv",
                           header = TRUE,
                           sep = ",")
+scatter <- ggplot(df_real_estate,
+                  aes(x = Area..ft..,
+                      y = Price)) + geom_point(aes(color = factor(Building.Type),
+                                                   size = 2),
+                                               alpha = 0.4) + 
