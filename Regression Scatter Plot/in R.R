faf6c5cfcf7f208e8d678671ab844c13ac11944d
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 16:18:48 +0300

    Update in R.R

--- Regression Scatter Plot/in R.R
+++ Regression Scatter Plot/in R.R
@@ -8,3 +8,13 @@ regression_scatter <- ggplot(df_ad_expenditure,
                              aes(x = Budget,
                                  y = Sales)) +
                       geom_point(size = 3,
+                                 color = 'grey12') + 
+                      geom_smooth(method = lm,
+                                  color = 'red',
+                                  fill = 'red') + #, se = FALSE
+  theme_classic() + 
+  xlab("Ad Expediture in (000's $)") +
+  ylab("Sales in (000's Units") +
+  ggtitle("Effect of Ad Expenditure on Sales")
+
+regression_scatter                                 
