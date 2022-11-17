74ec92f408aa172dc9c8c6146450aa1fa6ed1d46
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-17 14:23:12 +0300

    Update in R.R

--- Scatter Plot/in R.R
+++ Scatter Plot/in R.R
@@ -9,3 +9,9 @@ scatter <- ggplot(df_real_estate,
                       y = Price)) + geom_point(aes(color = factor(Building.Type),
                                                    size = 2),
                                                alpha = 0.4) + 
+                                    guides(size = FALSE) + 
+                                    labs(color = "Building Type") + 
+                                    scale_color_manual(values = wes_palette(name = "Darjeeling1", n = 5)) + 
+                                    theme(legend.justification = c(0.01,1),
+                                          legend.position = c(0.01,1)) + 
+       
