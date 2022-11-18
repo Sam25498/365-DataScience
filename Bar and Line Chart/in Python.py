c496723bdfb04a06f7e7cea86fc316e56ae12247
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-18 12:31:59 +0300

    Update in Python.py

--- Bar and Line Chart/in Python.py
+++ Bar and Line Chart/in Python.py
@@ -9,8 +9,14 @@ fig, ax = plt.subplots(figsize = (10,7))
 ax.bar(df_kdnuggets["Year"],
        df_kdnuggets["Participants"],
        color="k")
+ax.set_ylabel("Number of Participants", 
+              weight = "bold")
+ax.tick_params(axis = "y",
+               width = 2,
+               labelsize = "large")
 ax1 = ax.twinx()
-
+ax1.set_ylim(0,1)
+ax1.yaxis.set_major_formatter(PercentFormatter(xmax = 1.0))
 ax1.plot(df_kdnuggets["Year"],
        df_kdnuggets["Python Users"],
          color = "#b60000",
