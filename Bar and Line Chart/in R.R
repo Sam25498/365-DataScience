dbe653e8312c9c13d6bca59a6867e50e309e4dd4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-18 12:55:52 +0300

    Update in R.R

--- Bar and Line Chart/in R.R
+++ Bar and Line Chart/in R.R
@@ -4,3 +4,10 @@ df_kdnuggets <- read.csv("C:/Users/sbyeg/Downloads/bar_line_chart_data.csv",
                          header = TRUE,
                          sep = ",")
 
+combo <- ggplot(df_kdnuggets,
+                aes(x = Year,
+                    y = Participants, Python.Users)) +
+        geom_bar(aes(y = Participants),
+                 stat = "identity",
+                 fill = "black") +
+        geom_line(aes(y = Python.Users*max(Participants)),
